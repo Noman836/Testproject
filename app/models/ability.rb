224133320c01aa48b379c9c1a026d2c7647b8ecc
Noman836 <rao977684@gmail.com>
2023-07-13 02:30:17 -0700

    can can auth

---
+++ app/models/ability.rb
@@ -0,0 +1,13 @@
+# frozen_string_literal: true
+
+class Ability
+  include CanCan::Ability
+  def initialize(user)
+    user ||= User.new # guest user (not logged in)
+    if user.admin?
+      can :manage, Post
+    else
+      can :read, Post
+    end
+  end
+ end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  root 'posts#index'
+  resources :posts
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
-
+  devise_for :users
+  devise_scope :user do
+  get '/settings' => 'registrations#edit'
+  end
   # Defines the root path route ("/")
   # root "articles#index"
+ 
+
 end
